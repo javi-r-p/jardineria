3318e2f9c6285752362b2acbf4e2e1324efb14ad
Javier Rodríguez Parra <117043352+javi-r-p@users.noreply.github.com>
2024-11-20 11:12:31 +0100

    Añadida la fuente y clases CSS iniciales

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 	<head>
 		<title>Jardinería</title>
 		<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/general.css">
+		<link rel="stylesheet" href="css/index.css">
 		<?php
 			require('conexion.php');
 			$consulta = mysqli_query($conexion, "SELECT * FROM Productos")
 		?>
 	</head>
 	<body>
+		<h1>Tienda online de productos de jardinería</h1>
 		<?php
-			while ($productos = mysqli_fetch_array($consulta)) {
-				echo $productos['CodigoProducto'] . "<br>";
-			}
 		?>
 	</body>
 </html>
