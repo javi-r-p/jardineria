1f89d10e48c24ff6112c6cdc43e9e32a8ec2b748
Javier Rodríguez Parra <117043352+javi-r-p@users.noreply.github.com>
2024-11-21 14:32:21 +0100

    Modificada la apariencia

--- productos.php
+++ productos.php
@@ -11,9 +11,11 @@
 		?>
 	</head>
 	<body>
-		<h1>Catálogo de productos</h1>
-		<button class="botonApertura" onclick="abrirMenu()">&#9776;</button>
-		<input type="text" placeholder="Introduce un producto a buscar">
+		<section id="cuerpo" class="cuerpo">
+			<h1>Catálogo de productos</h1>
+			<button class="botonApertura" onclick="abrirMenu()">&#9776;</button>
+			<input type="text" placeholder="Introduce un producto a buscar">
+		</section>
 		<aside class="sidenav" id="menu">
 			<section><a class="botonCierre" onclick="cerrarMenu()">&#215;</a></section>
 			<section><a href="login.php">Iniciar sesión</a></section>
@@ -21,12 +23,14 @@
 		</aside>
 		<?php
 			while ($productos = mysqli_fetch_array($consulta)) {
-				echo "<section>\n";
+				echo "<section class='articulo'>\n";
 				echo "<img src='imagenes/" . $productos['CodigoProducto'] . ".jpg' alt='Imagen de " . $productos['CodigoProducto'] . "'>\n";
 				echo "<a href='producto.php?codigoProducto=" . $productos['CodigoProducto'] . "'>" . $productos['Nombre'] . "</a>\n";
 				echo "<p>" . $productos['PrecioVenta'] . " &#8364;</p>\n";
 				echo "</section>\n";
 			}
+			mysqli_free_result($consulta);
+			mysqli_close($conexion);
 		?>
 	</body>
 </html>
