9d50f1bc4d8790df91a315a3ab632e26be3c7371
Javier Rodríguez Parra <117043352+javi-r-p@users.noreply.github.com>
2024-11-21 12:25:20 +0100

    Solucionados errores de apariencia.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function abrirMenu() {
     document.getElementById("menu").style.width = "20%";
+    document.getElementById("menu").style.display = "block";
     document.getElementById("cuerpo").style.width = "80%";
 }
 function cerrarMenu() {
     document.getElementById("menu").style.width = "0%";
+    document.getElementById("menu").style.display = "none";
     document.getElementById("cuerpo").style.width = "100%";
 }
